6761ccb190fe11dad1526b5b3941f7e0da7e1501
Sebastian Skuse <seb@skuse.net>
2012-02-01 15:05:05 +0000

    Moved wiktionary parsing code server-side
    Signed-off-by: Sebastian Skuse <seb@skuse.net>

--- dictionary.js
+++ dictionary.js
@@ -44,78 +44,15 @@
 			}
 
 		});
-
-		AtKit.addFn('parseDictionaryResponse', function(input){
-	
-			// Remove translations blocks.
-			output = input.replace(/^((?:={2,})+translations(?:={2,})+.*?)((?:={2,}).*?(?:={2,})|(?:-{4}))/ig, '$2');	
-			
-			// Replace headings.
-			var output = output.replace(/(={2,})+(.*?)(?:={2,})+/ig, function(match, g1, g2, position, input) {
-		    	return "<h" + g1.length + ">" + g2 + "</h" + g1.length + ">";
-		    });
-		    
-		    // Remove comments
-		    var output = output.replace(/(<!--.*?-->)/ig, '');
-			
-			// Replace bold / italics.
-			output = output.replace(/(('+){1}(.*?)(?:'+){1})/ig, function(match, g1, g2, g3, position, input){
-				switch(g2.length){
-				
-					case 2:
-						return "<em>" + g3 + "</em>";
-					break;
-					
-					case 3:
-						return "<b>" + g3 + "</b>";
-					break;
-					
-					case 5:
-						return "<em><b>" + g3 + "</b></em>";
-					break;
-				}
-			
-			});
-			
-			// Replace text in curley brackets.
-			output = output.replace(/(\{\{(?:(.*?)\|)+(.*?)\}\})/ig, function(match, g1, g2, g3, position, input){
-				switch(g2.toLowerCase()){
-					case 'also':
-						return "See also: " + g3;
-					break;
-					
-					case 'ipa':
-						return g3;
-					break;
-					
-					case 'audio':
-						return "";
-					break;
-					
-					default:
-						return g3;	
-					break;
-				}
-			});
-			
-			// Replace lists.
-			output = output.replace(/([#|\*]+(.*?)\n)/ig, '<li>$2</li>');
-			
-			// Replace unmatched doublebraces.
-			output = output.replace(/(\{\{(\w{1,})\}\})/ig, "<i>$2</i>");
-			
-			output = output.replace(/(\[\[(.*?)\]\])/ig, "$2");
-			
-			return output;
-	
-		});
 		
 		AtKit.addButton(
 			'dictionary', 
 			AtKit.localisation("dictionary_title"),
 			AtKit.getPluginURL() + 'images/book_open.png',
 			function(dialogs, functions){
-				var data = eval("\"" + AtKit.call('getSelectedText', true) + "\";");
+				var text = AtKit.call('getSelectedText');
+				
+				var data = eval("\"" + text.split(" ").slice(0, 1) + "\";");
 				
 				if(data != ""){
 					$lib("#at-lnk-dictionary").children('img').attr('src', AtKit.getPluginURL() + "images/loading.gif");
@@ -126,22 +63,19 @@
 							if(result > -1){
 								var definition = eval("ro.query.pages[\"" + result + "\"].revisions[0][\"*\"];");
 								var title = eval("ro.query.pages[\"" + result + "\"].title;");
-								// Format the wikicode into something we can read in HTML.
-								//console.log(definition);
-								
-								definition = AtKit.call('parseDictionaryResponse', definition);
+
 							} else {
 								var definition = "Unknown word";
 								var title = eval("ro.query.pages[\"-1\"].title;");
 							}
 						}
 						
-						AtKit.message("<h2>" + AtKit.localisation("dictionary_definition") + " \"" + title + "\"</h2><div class=\"constrainContent\">" + definition + "</div>");
+						AtKit.message("<h2>" + AtKit.localisation("dictionary_definition") + " \"" + title + "\"</h2><div style=\"height:300px; overflow-x:scroll\">" + definition + "</div>");
 						$lib("#at-lnk-dictionary").children('img').attr('src', AtKit.getPluginURL() + "images/book_open.png");
 					});
 					
 				} else {
-					$lib.facebox("<h2>" + AtKit.localisation("dictionary_title") + "</h2><p>" + AtKit.localisation("dictionary_use") + "</p>");
+					AtKit.message("<h2>" + AtKit.localisation("dictionary_title") + "</h2><p>" + AtKit.localisation("dictionary_use") + "</p>");
 					$lib("#at-lnk-dictionary").children('img').attr('src', AtKit.getPluginURL() + "images/book_open.png");
 				}
 			},
