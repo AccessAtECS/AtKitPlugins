5f9432af975fbf6217b38d4d13d5f311d9f68611
Sebastian Skuse <seb@skuse.net>
2012-02-06 15:18:27 +0000

    Magnus' plugins
    Signed-off-by: Sebastian Skuse <seb@skuse.net>

---
+++ ff.js
@@ -0,0 +1,63 @@
+(function(){
+
+	var pluginName = "ff";
+	var plugin = function(){
+		
+		$lib = AtKit.lib();
+		
+		settings = {
+			"baseURL": "http://c.atbar.org/ATBar/"
+		};
+		
+		if(/https:/.test(window.location.protocol)){
+			settings.baseURL = "https://ssl.atbar.org/c/ATBar/";
+		}
+		
+		// Internationalisation
+		AtKit.addLocalisationMap("GB", {
+			"ff_title" : "Start FontFriend"
+		});
+		
+		// FontFriend
+		AtKit.addButton(
+			'ff', 
+			AtKit.localisation("ff_title"),
+			AtKit.getPluginURL() + 'images/font.png',
+			function(dialogs, functions){
+
+				if(typeof jQuery=='undefined'){
+					var jqit=document.createElement('script');jqit.type='text/javascript';jqit.src='http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';document.getElementsByTagName('head')[0].appendChild(jqit);
+				}
+				
+				_my_script=document.createElement('script');_my_script.type='text/javascript';_my_script.src='http://font-friend.googlecode.com/svn/trunk/font-friend.js';document.getElementsByTagName('head')[0].appendChild(_my_script);
+
+
+			},
+			null, null
+		);
+
+	};
+
+	if(typeof window['AtKit'] == "undefined"){
+
+		window.AtKitLoaded = function(){
+			var eventAction = null;
+		
+			this.subscribe = function(fn) {
+				eventAction = fn;
+			};
+		
+			this.fire = function(sender, eventArgs) {
+				if (eventAction != null) {
+					eventAction(sender, eventArgs);
+				}
+			};
+		}
+
+		window['AtKitLoaded'] = new AtKitLoaded();
+		window['AtKitLoaded'].subscribe(function(){ AtKit.registerPlugin(pluginName, plugin); });
+	} else {
+		AtKit.registerPlugin(pluginName, plugin);
+	}
+
+})();
---
+++ google.js
@@ -0,0 +1,67 @@
+(function(){
+
+	var pluginName = "google";
+	var plugin = function(){
+		
+		$lib = AtKit.lib();
+		
+		settings = {
+			"baseURL": "http://c.atbar.org/ATBar/"
+		};
+		
+		if(/https:/.test(window.location.protocol)){
+			settings.baseURL = "https://ssl.atbar.org/c/ATBar/";
+		}
+		
+		// Internationalisation
+		AtKit.addLocalisationMap("GB", {
+			"google_title" : "Google search"
+		});
+
+		// Google
+		AtKit.addButton(
+			'google', 
+			AtKit.localisation("google_title"),
+			AtKit.getPluginURL() + 'images/google.png',
+			function(dialogs, functions){
+
+
+				q = "" + (window.getSelection ? window.getSelection() : document.getSelection ? document.getSelection() : document.selection.createRange().text); 
+				
+				if (!q) {
+					q = prompt("You did not select any text.  Enter a search phrase:", ""); 
+				}
+				if (q!=null) {
+					location="http://www.google.com/search?q=" + escape(q).replace(/ /g, "+"); 
+				}
+				void 0
+			
+			},
+			null, null
+		);
+
+	};
+
+	if(typeof window['AtKit'] == "undefined"){
+
+		window.AtKitLoaded = function(){
+			var eventAction = null;
+		
+			this.subscribe = function(fn) {
+				eventAction = fn;
+			};
+		
+			this.fire = function(sender, eventArgs) {
+				if (eventAction != null) {
+					eventAction(sender, eventArgs);
+				}
+			};
+		}
+
+		window['AtKitLoaded'] = new AtKitLoaded();
+		window['AtKitLoaded'].subscribe(function(){ AtKit.registerPlugin(pluginName, plugin); });
+	} else {
+		AtKit.registerPlugin(pluginName, plugin);
+	}
+
+})();
